e649997da34336b85d50f47745cc2677633a7059
JGHolscher <holschjg@mail.uc.edu>
2022-09-15 14:03:48 -0400

    no

--- src/CollectAll.java
+++ src/CollectAll.java
@@ -1,9 +1,12 @@
-public class CollectAll {
-    public static void main(String[] args){ //maybe not this line hopefully confusion
-    //if this does work put the method in each of the main file
-//Write a method to use in your main program:  collectAll(…)
-//That returns all objects in the objects array that are accepted by the given filter.
+public class CollectAll
+{
+//return all objects in the objects array that are accepted by the given filter.
 
 
-    }
+        //Method. Collectall(). Return objects, accepted by filter.
+        public static void CollectAll(Object x) {
+            // body
+        }
+
 }
+
--- src/ShortWordFilter.java
+++ src/ShortWordFilter.java
@@ -1,4 +1,11 @@
-public class ShortWordFilter {
+public class ShortWordFilter implements Filter{
+
+
+
+    @Override
+    public boolean accept(Object x) {
+        return false;
+    }
     //implements the Filter interface
     // whose accept method
     // accepts all strings of length < 5.
